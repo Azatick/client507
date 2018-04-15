b4ea62617837144c2917fb0ddac1085a49a5588b
Aidar Akhmetow <doratio@MacBook-Pro-Aidar.local>
2018-04-16 00:14:14 +0300

    add profile

--- src/vue/components/abstract/index.ts
+++ src/vue/components/abstract/index.ts
@@ -2,10 +2,12 @@ import Button from './Button.vue'
 import Divider from './Divider.vue'
 import MForm from './MForm.vue'
 import MInput from './MInput.vue'
+import MBlock from './MBlock.vue'
 
 export default {
 	Button,
 	Divider,
 	MForm,
-	MInput
+	MInput,
+	MBlock
 }
